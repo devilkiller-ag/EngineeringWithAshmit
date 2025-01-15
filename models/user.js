d10cc3b7bb8a9d1cf9f925bd46dedcba98a43aa7
devilkiller-ag <ashmitgupta.official@gmail.com>
2025-01-16 04:03:34 +0530

    feat: User model

---
+++ models/user.js
@@ -0,0 +1,55 @@
+const { Schema, Model } = require('mongoose');
+const { createHmac, randomBytes } = require('crypto');
+
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: `/images/avatars/avatar_${Math.floor((Math.random() * 6) + 1)}.png`,
+    },
+    role: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER',
+    }
+}, { timestamps: true });
+
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) return next();
+
+    const salt = randomBytes(16).toString();
+    const hashPassword = createHmac('sha256', salt)
+        .update(user.password)
+        .digest('hex');
+
+    this.salt = salt;
+    this.password = hashPassword;
+
+    next();
+})
+
+
+const User = Model('User', userSchema);
+
+
+module.exports = User;
