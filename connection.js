5e9cefaee3109376d835f993c2092b1520e81256
devilkiller-ag <ashmitgupta.official@gmail.com>
2025-01-17 04:00:53 +0530

    icreased mongo db timing

--- connection.js
+++ connection.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async (url) => {
     return mongoose
-        .connect(url)
+        .connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000, // 30 seconds
+            socketTimeoutMS: 45000, // 45 seconds
+        })
         .then(() => console.log('Connected to MongoDB'))
         .catch((err) => console.log('MongoDB error: ', err));
 };
